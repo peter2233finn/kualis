70967b307fd82be5bbdef54cdfbac959f7e5ef26
Peter Finn <peter123finn@gmail.com>
2025-04-18 17:03:49 +0100

    Update vulnfinder.sh
    Changing function format. Need to write code

--- vulnfinder.sh
+++ vulnfinder.sh
@@ -1,6 +1,9 @@
-
-"LOW: Trace method allowed" Access-Control-Allow-Methods + TRACE
-"LOW: Trace method allowed" Access-Control-Allow-Methods + TRACK
-"LOW: Trace method allowed" Access-Control-Allow-Methods + OPTIONS
+# 1 - issue
+# 2 - file
+# 3+ - actions to grep. + means add
+"INFO: SSH Version detected" nmap-ssh-no-brute "OpenSSH "
+"LOW: Trace method allowed" curl-to-root Access-Control-Allow-Methods + TRACE
+"LOW: Trace method allowed" curl-to-root Access-Control-Allow-Methods + TRACK
+"LOW: Trace method allowed" curl-to-root Access-Control-Allow-Methods + OPTIONS
 
 
