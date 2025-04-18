e8da7d22ef0c525b22b657afbda07d02a5ed25a6
Peter Finn <peter123finn@gmail.com>
2025-04-18 17:33:51 +0100

    Update vulnfinder.sh
    Getting there

--- vulnfinder.sh
+++ vulnfinder.sh
@@ -1,9 +1,32 @@
+#! /bin/bash
 # 1 - issue
-# 2 - file
-# 3+ - actions to grep. + means add
-"INFO: SSH Version detected" nmap-ssh-no-brute "OpenSSH "
-"LOW: Trace method allowed" curl-to-root Access-Control-Allow-Methods + TRACE
-"LOW: Trace method allowed" curl-to-root Access-Control-Allow-Methods + TRACK
-"LOW: Trace method allowed" curl-to-root Access-Control-Allow-Methods + OPTIONS
+# 2 - initial grep. Will run Grep -R $2 folder
+# 3+ - actions to grep. This will be added as " | grep $this"
+
+folder="$1"
+
+function simpleFind () {
+
+        # find number of issues to grep
+        lock=0
+        toRun="grep -R \"$2\" $folder"
+        for i in "$@"; do
+                if [ $lock -gt 1 ]; then
+                        toRun+=" | grep \"$i\""
+                fi
+                ((lock++))
+        done
+        bash -c "$toRun" > /tmp/vulnFinder.tmp
+
+        if [ -s /tmp/vulnFinder.tmp ]; then
+                printf "\n\n$1"
+                cat /tmp/vulnFinder.tmp
+        fi
+
+        #grep -Ri "$folder" $2
+}
 
 
+simpleFind "INFO: SSH Version detected" "OpenSSH " "nmap-ssh-no-brute" 
+simpleFind "LOW: Trace method allowed" "Access-Control-Allow-Methods" "curl-to-root"
+simpleFind "INFO: Server header in use" "Server: " "curl-to-root"
