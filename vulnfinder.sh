a9623d9ee0f6589007ef000a0b304add12d3a448
Peter Finn <c16407722@mydit.ie>
2025-04-18 14:45:16 +0100

    Glodified grepper. Need to add code

---
+++ vulnfinder.sh
@@ -0,0 +1,6 @@
+
+"LOW: Trace method allowed" Access-Control-Allow-Methods + TRACE
+"LOW: Trace method allowed" Access-Control-Allow-Methods + TRACK
+"LOW: Trace method allowed" Access-Control-Allow-Methods + OPTIONS
+
+
