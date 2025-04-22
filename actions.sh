fe74c18d7682198d8fe6eebcc8da6df3e6fc4d6e
Peter Finn <peter123finn@gmail.com>
2025-04-22 15:08:33 +0100

    Update actions.sh
    Tweaks and removed issueRed function as vulnFind does this now is a more modular and better way

--- actions.sh
+++ actions.sh
@@ -1,35 +1,6 @@
 #! /bin/bash
 brute=true
 
-function issueList {
-	# $1 complete file
-	# $2 string to search
-	# $3 text to add
-	tmpFile="/tmp/$(tr -dc A-Za-z0-9 </dev/urandom | head -c 23; echo).pScan.issue.Find"
-	touch "$(echo "$1" | tr '/' ' '| awk '{$NF=""}1' | tr ' ' '/' )/issues" 2> /dev/null
-	
-	string=$(grep -E -i "$2" "$1")
-       	echo $string > $tmpFile
-	
-	if [ -z "$string" ]; then 
-		echo "$3 $(cat $tmpFile)" | tee -a "$(echo "$1" | tr '/' ' '| awk '{$NF=""}1' | tr ' ' '/' )/issues" > /dev/null
-	fi
-	rm $tmpFile
-}
-
-function issueRed {
-	# $1 complete file
-	# $2 text to add
-	tmpFile="/tmp/$(tr -dc A-Za-z0-9 </dev/urandom | head -c 23; echo).pScan.issue.Find"
-	touch "$(echo "$1" | tr '/' ' '| awk '{$NF=""}1' | tr ' ' '/' )/issues" 2> /dev/null
-	string=$(cat "$1" | sed -n '/\x1b\[31m/p')
-       	echo $string > $tmpFile
-	if [ -z "$string" ]; then 
-		echo "$2 $(cat $tmpFile)"| tee -a "$(echo "$1" | tr '/' ' '| awk '{$NF=""}1' | tr ' ' '/' )/issues" > /dev/null
-	fi
-	rm $tmpFile
-}
-
 function http-proxy-function {
 	ip=$1; port=$2; folder=$3
 	targets=(
@@ -64,20 +35,19 @@ function http-function {
 	blacklist=$(cat "${folder}curl-to-random-directory" | grep '< HTTP/' | awk '{print $3}')
 	timeout 1900 gobuster dir -k -u http://${ip}:${port} -w /usr/share/dirb/wordlists/common.txt -b ${blacklist} -o "${folder}gobuster" 2> /dev/null > /dev/null
 	timeout 1900 nikto -port ${port} -Tuning 01234abcx57896 -host http://${ip} -Plugins headers outdated httpoptions robots origin_reflection put_del_test shellshock cgi docker_registry favicon apacheusers msgs report_text content_search parked paths tests 2>&1 > "${folder}nikto"
-	issueList "${folder}nikto" "header is not" "Missing HTTP Header: "
 }
 
 
 function telnet-function {
 	ip=$1; port=$2; folder=$3
-	nmap ${ip} -p ${port} --script "telnet* and not brute" > "${folder}/nmap-telnet-no-brute"
-	[ $brute = true ] && nmap -Pn ${ip} -p ${port} -sV --script="telnet-brute"  > "${folder}nmap-telnet-brute-force"
+	nmap -Pn ${ip} -p ${port} --script "telnet* and not brute" > "${folder}/nmap-telnet-no-brute" 2>&1
+	[ $brute = true ] && nmap -Pn ${ip} -p ${port} -sV --script="telnet-brute"  > "${folder}nmap-telnet-brute-force" 2> /dev/null
 }
 
 
 function ssh-function {
 	ip=$1; port=$2; folder=$3
-	ssh-audit ${ip} -p ${port} > "${folder}ssh-audit"
+	ssh-audit ${ip} -p ${port} > "${folder}ssh-audit" 2>&1
 	nmap -Pn ${ip} -p ${port} -sV --script="ssh* and not brute"  > "${folder}nmap-ssh-no-brute"
 	[ $brute = true ] && nmap -Pn ${ip} -p ${port} -sV --script="ssh-brute"  > "${folder}nmap-ssh-brute-force"
 }
@@ -90,22 +60,20 @@ function https-function {
 	
 	timeout 1900 gobuster dir -k -u https://${ip}:${port} -b ${blacklist} -w /usr/share/dirb/wordlists/common.txt -o "${folder}gobuster" 2> /dev/null > /dev/null
 	timeout 1900 nikto -port ${port} -Tuning 01234abcx57896 -host https://${ip} -Plugins headers outdated httpoptions robots origin_reflection put_del_test shellshock cgi docker_registry favicon apacheusers msgs report_text content_search parked paths tests 2>&1 > "${folder}nikto"
-	issueList "${folder}nikto" "header is not" "Missing HTTP Header: "
 	
 }
 
 function ssl-function {
 	ip=$1; port=$2; folder=$3
 	sslscan ${ip}:${port} > "${folder}sslscan"
-	issueRed "${folder}sslscan" "SSL Issue: "
 }
 
 function smtp-function {
 	ip=$1; port=$2; folder=$3
 	timeout 60 sendmail -t test@test.com -f test@test.com -s ${ip} -u "Testing SMTP Relay" 2>&1 > "${folder}SMTP-Relay-test"
-	nmap ${ip} -p ${port} --script="*smtp* and not brute" -Pn  > "${folder}nmap-smtp-scripts"
-	[ $brute = true ] && nmap ${ip} -p ${port} --script="smtp-brute" -Pn  > ${folder}nmap-smtp-brute-scripts"
-	smtp-user-enum -t ${ip} -p ${port} -U /usr/share/wordlists/common-snmp-community-strings.txt  > ${folder}smtp-user-enum"
+	nmap ${ip} -p ${port} --script="*smtp* and not brute" -Pn  > "${folder}nmap-smtp-scripts" 2>&1
+	[ $brute = true ] && nmap ${ip} -p ${port} --script="smtp-brute" -Pn  > "${folder}nmap-smtp-brute-scripts" 2> /dev/null
+	smtp-user-enum -t ${ip} -p ${port} -U /usr/share/wordlists/common-snmp-community-strings.txt  > "${folder}smtp-user-enum" 2>&1
 }
 
 function snmp-function {
@@ -119,9 +87,9 @@ function snmp-function {
 
 function mysql-function {
 	ip=$1; port=$2; folder=$3
-	nmap ${ip} -p ${port} -Pn --script="mysql* and not brute" > ${folder}nmap-mysql-scripts
-	[ $brute = true ] && nmap ${ip} -p ${port} -Pn --script="mysql-brute" > ${folder}nmap-mysql-brute
-	[ $brute = true ] && hydra -L /usr/share/wordlists/usernames.txt -P /usr/share/wordlists/rockyou.txt ${ip} mysql -I -s ${port} -o ${folder}mysql-hydra
+	nmap ${ip} -p ${port} -Pn --script="mysql* and not brute" > ${folder}nmap-mysql-scripts 2>/dev/null
+	[ $brute = true ] && nmap ${ip} -p ${port} -Pn --script="mysql-brute" > ${folder}nmap-mysql-brute 2> /dev/null
+	[ $brute = true ] && timeout 9000 hydra -L /usr/share/wordlists/usernames.txt -P /usr/share/wordlists/rockyou.txt ${ip} mysql -I -s ${port} -o ${folder}mysql-hydra 2> /dev/null
 }
 
 function telnet-function {
@@ -133,60 +101,58 @@ function telnet-function {
 
 function unknown-function {
 	ip=$1; port=$2; folder=$3
-	nmap -sV -sC ${ip} -p ${port} --script="*" > ${folder}nmap-unknown 2>&1
-	nmap -sV -sC ${ip} -p ${port} --version-all > ${folder}nmap-all-probes 2>&1
+	nmap -Pn -sV -sC ${ip} -p ${port} --script="*" > ${folder}nmap-unknown 2>&1
+	nmap -Pn -sV -sC ${ip} -p ${port} --version-all > ${folder}nmap-all-probes 2>&1
 	curl --insecure https://${ip}:${port} > ${folder}unknown-curl-https 2>&1
 	curl http://${ip}:${port} > ${folder}unknown-curl-http 2>&1
 }
 
 function msrcp-function { 
 	ip=$1; port=$2; folder=$3
-	timeout 300 rpcdump.py ${ip} > ${folder}rpcdump-without-port
-	timeout 300 rpcdump.py ${ip} -p ${port} > ${folder}rpcdump-to-port
+	timeout 300 rpcdump.py ${ip} > ${folder}rpcdump-without-port 2>&1
+	timeout 300 rpcdump.py ${ip} -p ${port} > ${folder}rpcdump-to-port 2>&1
 }
 
 function mssql-function {
 	ip=$1; port=$2; folder=$3
-	[ $brute = true ] && nmap ${ip} -p ${port} -Pn --script="ms-sql-brute" > ${folder}/mssql-brute-nmap
+	nmap ${ip} -p ${port} -Pn --script="ms-sql* and not brute" > ${folder}/mssql-nmap-scripts 2>&1
+	[ $brute = true ] && nmap ${ip} -p ${port} -Pn --script="ms-sql-brute" > ${folder}/mssql-brute-nmap 2>&1
 }
 
 function microsoft-ds-function {
 	ip=$1; port=$2; folder=$3
-	nmap -sV -sC ${ip} -p ${port} --script="smb*" -Pn > ${folder}smb-nmap-scripts
+	nmap -sV -sC ${ip} -p ${port} --script="smb*" -Pn > ${folder}smb-nmap-scripts 2>&1
 	timeout 60 nbtscan -v -r ${ip} > ${folder}nbtscan 2>&1
-	[ $brute = true ] && hydra -t 1  -f -l administrator -P /usr/share/wordlists/rockyou.txt $ip smb
-	issueList "${folder}nbtscan" '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' "Possibly add to scope (from nbtscan): "
-	issueList "${folder}SMB-scripts-nmap" "dangerous, but default" "SMBv1 is enabled: "
+	[ $brute = true ] && timeout 9000 hydra -t 1  -f -l administrator -P /usr/share/wordlists/rockyou.txt $ip smb -o ${folder}/smb-hydra 2> /dev/null
 }
 
 
 function ftp-function {
 	ip=$1; port=$2; folder=$3
-	nmap -sV -sC -Pn ${ip} -p ${port} --script="*ftp* and not brute" > ${folder}ftp-nmap-scripts
-	[ $brute = true ] && nmap -sV -sC -Pn ${ip} -p ${port} --script="ftp-brute" > ${folder}ftp-nmap-scripts
+	nmap -sV -sC -Pn ${ip} -p ${port} --script="*ftp* and not brute" > ${folder}ftp-nmap-scripts 2>&1
+	[ $brute = true ] && nmap -sV -sC -Pn ${ip} -p ${port} --script="ftp-brute" > ${folder}ftp-nmap-scripts 2> /dev/null
 }
 
 function dns-function {
 	ip=$1; port=$2; folder=$3
-	nmap $ip -p $port -Pn --script="dns* and not brute" > ${folder}nmap-dns
- 	[ $brute = true ] && nmap $ip -p $port -Pn --script="dns-brute" > ${folder}dns-brute-nmap
+	nmap $ip -p $port -Pn --script="dns* and not brute" > ${folder}nmap-dns 2>&1
+ 	[ $brute = true ] && nmap $ip -p $port -Pn --script="dns-brute" > ${folder}dns-brute-nmap 2>&1
 
 }
 
 function rpc-function {
 	ip=$1; port=$2; folder=$3
-	nmap $ip -p $port -Pn --script="*rpc* and not brute" > ${folder}rpc-nmap-scripts
-	[ $brute = true ] && nmap ${ip} -p ${port} -Pn --script="rpcap-brute"  > ${folder}rpc-brute-nmap
+	nmap $ip -p $port -Pn --script="*rpc* and not brute" > ${folder}rpc-nmap-scripts 2>&1
+	[ $brute = true ] && nmap ${ip} -p ${port} -Pn --script="rpcap-brute"  > ${folder}rpc-brute-nmap 2>&1
 }
 
 function rdp-function {
 	ip=$1; port=$2; folder=$3
-	nmap $ip -p $port -Pn --script="*rdp* and not brute" > ${folder}rdp-nmap-scripts
-	[ $brute = true ] && nmap ${ip} -p ${port} -Pn --script="rdp-brute"  > ${folder}rdp-brute-nmap
+	nmap $ip -p $port -Pn --script="*rdp*" > ${folder}rdp-nmap-scripts 2>&1
 }
 function netbios-function {
 	ip=$1; port=$2; folder=$3
-	nmblookup -A ${ip} > ${folder}/nmblookup
-	nmap -sU -Pn --script="nb*" ${ip} > ${folder}/nmap-netbios-scripts
-	enum4linux -a ${ip} > ${folder}/enum4linux
+	nmblookup -A ${ip} > ${folder}/nmblookup 2>&1
+	nmap -sU -Pn --script="nb*" ${ip} > ${folder}/nmap-netbios-scripts 2>&1
+	enum4linux -a ${ip} > ${folder}/enum4linux 2>&1
 }
